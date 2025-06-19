7015c1a0cc4ee69c0ab5f7ea654c16788d74cf79
steffahv <tefavv22@gmail.com>
2025-06-19 12:04:42 -0500

    Refactor UI components

--- scripts/source_links.py
+++ scripts/source_links.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 AZURE_BLOB_CONN_STR = os.getenv("AZURE_BLOB_CONN_STR")
 AZURE_BLOB_CONTAINER = os.getenv("AZURE_BLOB_CONTAINER")
-AZURE_BLOB_BASE_URL = os.getenv("AZURE_BLOB_BASE_URL")  # e.g. https://<account>.blob.core.windows.net/<container>
+AZURE_BLOB_BASE_URL = os.getenv("AZURE_BLOB_BASE_URL")  
 
 # Connect to the container
 container_client = ContainerClient.from_connection_string(
